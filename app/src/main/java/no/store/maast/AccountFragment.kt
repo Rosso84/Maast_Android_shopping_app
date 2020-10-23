02a625072f0c32b74cec653af03e38491e5c4dc0
Rosso84 <Roosbeh@hotmail.com>
2020-10-23 12:21:55 +0200

    just for backup before renaming fragmentAccount..

--- app/src/main/java/no/store/maast/AccountFragment.kt
+++ app/src/main/java/no/store/maast/AccountFragment.kt
@@ -8,20 +8,54 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.fragment_account.*
 
 
 class AccountFragment : Fragment() {
 
+    private var mAuth: FirebaseAuth? = null
+    //public var user:
     lateinit var signUpFragment: SignUpFragment;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        onClick()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser: FirebaseUser? = mAuth?.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            Toast.makeText(activity, "Signed in as 'username", Toast.LENGTH_LONG).show()
+            //startActivity(Intent(this, AnotherActivity::class.java))
+        } else {
+            Toast.makeText(activity, "Click on Account to log in", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun onClick(): Void?{
         btn_forgotPass.setOnClickListener {
             Toast.makeText(
                 activity, "btn forgot pressed",
@@ -44,7 +78,6 @@ class AccountFragment : Fragment() {
         }
 
         btn_signup.setOnClickListener {
-
             signUpFragment = SignUpFragment();
             val transaction = activity?.supportFragmentManager?.beginTransaction()
             transaction?.replace(R.id.frame_layout, signUpFragment)
@@ -53,14 +86,7 @@ class AccountFragment : Fragment() {
 
         }
 
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        return null
     }
 
 }
--- app/src/main/java/no/store/maast/SignUpFragment.kt
+++ app/src/main/java/no/store/maast/SignUpFragment.kt
@@ -1,18 +1,22 @@
 package no.store.maast
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import java.lang.reflect.Field
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 class SignUpFragment : Fragment() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,20 +31,5 @@ class SignUpFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
-
-/*
-    override fun onDetach() {
-        super.onDetach()
-        try {
-            val childFragmentManager: Field =
-                Fragment::class.java.getDeclaredField("mChildFragmentManager")
-            childFragmentManager.setAccessible(true)
-            childFragmentManager.set(this, null)
-        } catch (e: NoSuchFieldException) {
-            throw RuntimeException(e)
-        } catch (e: IllegalAccessException) {
-            throw RuntimeException(e)
-        }
-    }*/
 
 }
